5ba88b5c2a1cb362d862a6096aa56e5dd68b08b4
ogy <ogy2658@naver.com>
2023-06-26 11:45:35 +0900

    [feat] 메인 레이아웃
    - 레이아웃 설정

--- src/main/java/icube/main/MainController.java
+++ src/main/java/icube/main/MainController.java
@@ -1,5 +1,28 @@
 package icube.main;
 
-public class MainController {
+import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import icube.common.framework.abst.CommonAbstractController;
+
+/**
+ * 랜딩 페이지
+ * @author ogy
+ *
+ */
+@Controller
+@RequestMapping(value="#{props['Globals.Main.path']}")
+public class MainController extends CommonAbstractController  {
+
+	@RequestMapping(value = {"","index"})
+	public String list(
+		HttpServletRequest request
+		, Model model
+			) throws Exception {
+		
+		return "/main/main";
+	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/main/layout/header.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!-- header -->
+<header id="header">
+	<h1 id="logo" class="global-logo">
+		<a href="#"><em>이로움ON</em></a>
+	</h1>
+	<nav id="navigation">
+		<ul>
+			<li><a href="#">로그인</a></li>
+			<!-- <li><a href="#">로그아웃</a></li> -->
+			<li><a href="#" class="join">회원가입</a></li>
+			<li><a href="#" class="cost">남은 금액보기</a></li>
+		</ul>
+	</nav>
+</header>
+<!-- //header -->
