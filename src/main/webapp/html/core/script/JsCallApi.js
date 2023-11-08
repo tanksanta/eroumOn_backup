1472991ff4640e2516714920496fd3b8e64789af
liveonDylee <101084764+liveonDylee@users.noreply.github.com>
2023-11-08 15:40:21 +0900

    JsCallApi.js 파일 추가

--- src/main/webapp/html/core/script/JsCallApi.js
+++ src/main/webapp/html/core/script/JsCallApi.js
@@ -1,8 +1,14 @@
 class JsCallApi{
-    constructor(waiting, alert, confirm) {
+    constructor() {
+        this._cls_info = {jsWaiting:(new JsWaiting()), call_cnt:0};
+        
     }
     call_api_get = function (owner, uri, callback, data, param){
-    
+        var _self = this;
+
+        _self._cls_info.call_cnt += 1;
+        _self._cls_info.jsWaiting.fn_show_popup();
+
         $.ajax({
             url: uri,
             data: data,
@@ -10,17 +16,27 @@ class JsCallApi{
             param: param,
             callback:callback,
         }).done(function(result){
+            _self.fn_callbacked();
             if (owner != null && owner != undefined && owner[callback] != null && owner[callback] != undefined){
-                owner[callback](data, param, result);
+                owner[callback](result, null, data, param);
             }
+        }).fail(function(result, status, err) {
+            _self.fn_callbacked();
+            if (owner != null && owner != undefined && owner[callback] != null && owner[callback] != undefined){
+                owner[callback](null, result, data, param);
+            }
+            // alert("배송지 삭제 중 오류가 발생했습니다. \n 관리자에게 문의 바랍니다.");
+            // console.log('error forward : ' + data);
         });
     };
 
     
-    call_svr_post_json = function (owner, uri, callback, data, param){
+    call_api_post_json = function (owner, uri, callback, data, param){
         var _self = this;
 
-        // _self.createProgressLoading();
+        _self._cls_info.call_cnt += 1;
+        _self._cls_info.jsWaiting.fn_show_popup();
+
         $.ajax({
             url: uri,
             data: data,
@@ -29,15 +45,16 @@ class JsCallApi{
             dataType : 'json',
             callback:callback,
         }).done(function(result){
+            _self.fn_callbacked();
             if (owner != null && owner != undefined && owner[callback] != null && owner[callback] != undefined){
-                owner[callback](data, param, result);
+                owner[callback](result, null, data, param);
             }
-        }).fail(function(data, status, err) {
-            // alert("배송지 삭제 중 오류가 발생했습니다. \n 관리자에게 문의 바랍니다.");
-            console.log('error forward : ' + data);
-        }).always(function(){/* result, textStatus, xhr */
-            // _self.removeProgressLoading();
-            console.log('JsCallApi call_svr_post_json always called');
+        }).fail(function(result, status, err) {
+            _self.fn_callbacked();
+            if (owner != null && owner != undefined && owner[callback] != null && owner[callback] != undefined){
+                owner[callback](null, result, data, param);
+            }
+            // console.log('error forward : ' + data);
         });
     };
 
@@ -85,21 +102,74 @@ class JsCallApi{
         return arr.join('&')
     }
 
-    //로딩바 생성
-	createProgressLoading() {
-		var template = `<div class="progress-loading is-dark">
-			<div class="icon">
-				<span></span><span></span><span></span>
-			</div>
-			<p class="text">데이터를 불러오는 중입니다.</p>
-		</div>`
-		
-		$('.cont-target').html(template);		
-	}
-
-    removeProgressLoading(){
-        $('.progress-loading').remove();
+    fn_callbacked(){
+        this._cls_info.call_cnt -= 1;
+        if (this._cls_info.call_cnt <= 0){
+            this._cls_info.jsWaiting.fn_close_popup();
+        }
     }
 }
 
-var jsCallApi = new JsCallApi();
+class JsWaiting{/*생성 하지 마시오~ JsCallApi에서 들고가기...*/
+    constructor(){
+        this._cls_info = this._cls_info || {joContent:null, joParent:null};
+
+        this._cls_info.pagePrefixParent = 'body';
+        
+    }
+    fn_show_popup(step){
+        if (this._cls_info.joContent == undefined){
+            this.createProgressLoading();
+
+            
+        }
+
+        this._cls_info.joParent.addClass('overlay-wait1');
+
+        this._cls_info.joContent.removeClass('off');
+
+        this.fn_show_overlay();
+
+    }
+    
+    fn_show_overlay(){
+        this._cls_info.joParent.addClass('overlay-wait1');
+    }
+    fn_close_popup(){
+        this._cls_info.joContent.addClass('off');
+        this._cls_info.joParent.removeClass('overlay-wait1');
+    }
+    
+    //로딩바 생성
+	createProgressLoading() {
+        if (this._cls_info.joParent == undefined || this._cls_info.joParent.length < 1){
+            this._cls_info.joParent = $(this._cls_info.pagePrefixParent);
+        }
+
+		
+		
+        if (this._cls_info.joParent != undefined && this._cls_info.joParent.length > 0){
+            
+            this._cls_info.joContent = $(this._cls_info.pagePrefixParent + " div.progress-loading");
+
+            if (this._cls_info.joContent == undefined || this._cls_info.joContent.length < 1){
+                var template = `<div class="progress-loading lazy is-dark">
+                    <div class="icon">
+                        <span></span><span></span><span></span>
+                    </div>
+                    <p class="text">데이터를 불러오는 중입니다.</p>
+                </div>`;
+
+                this._cls_info.joParent.append(template);
+                this._cls_info.joContent = $(this._cls_info.pagePrefixParent + " div.progress-loading");
+            }
+            
+        }
+		
+	}
+
+}
+
+
+var jsCallApi;
+jsCallApi = new JsCallApi();
