fa840da68f7faf2fb910efe42cb0548346a74638
liveonDylee <101084764+liveonDylee@users.noreply.github.com>
2023-11-15 15:35:10 +0900

    엑셀다운 사유, 패스워드 체크

--- src/main/java/icube/manage/popups/MPopupsController.java
+++ src/main/java/icube/manage/popups/MPopupsController.java
@@ -4,16 +4,23 @@ package icube.manage.popups;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import icube.common.framework.abst.CommonAbstractController;
+import icube.manage.sysmng.mngr.biz.MngrSession;
+import icube.manage.sysmng.mngr.biz.MngrVO;
+import icube.manage.sysmng.mngr.biz.MngrService;
+import icube.common.util.WebUtil;
 
 
 /**
@@ -26,25 +33,49 @@ import icube.common.framework.abst.CommonAbstractController;
 @Controller
 @RequestMapping(value="/#{props['Globals.Manager.path']}/api/popups")
 public class MPopupsController extends CommonAbstractController{
+    @Autowired
+	private MngrSession mngrSession;
+
+    @Resource(name = "mngrService")
+	private MngrService mngrService;
 
     /* 엑셀 다운로드 전 패스워드 확인*/
     @RequestMapping(value = {"excelPwd.json"})
     @ResponseBody
 	public Map<String, Object> excelPwd(
-			HttpServletRequest request,
-			HttpServletResponse response,
-			@RequestParam(value = "pwd", required=true) int pwd, 
-			@RequestParam(value = "txt", required=true) String txt, 
-			HttpSession session) throws Exception {
+        HttpServletRequest request,
+        HttpServletResponse response,
+        @RequestParam(value = "pwd", required=true) String pwd, 
+        @RequestParam(value = "txt", required=true) String txt, 
+        HttpSession session) throws Exception {
 
 		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("result", "1");
+		
+        if (!mngrSession.isLoginCheck()) {
+			resultMap.put("result", "fail");
+            return resultMap;
+		}
 
         /*패스워드 체크*/
+        pwd = WebUtil.clearSqlInjection(pwd);
+        
+        MngrVO mngrVO = mngrService.selectMngrByUniqueId(mngrSession.getUniqueId());
+
+        if (mngrVO == null) {
+            resultMap.put("result", "fail");
+            return resultMap;
+        }
+
+        if (!BCrypt.checkpw(pwd, mngrVO.getMngrPswd())) {
+            resultMap.put("result", "fail");
+            resultMap.put("failCd", "difference");
+            return resultMap;
+        }
 
         /*사유 저장하는 부분*/
 
 
+        resultMap.put("result", "OK");
 		return resultMap;
 	}
 }
