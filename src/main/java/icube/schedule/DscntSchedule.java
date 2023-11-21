bbdf5724453add90197b3cdbf6c1828a7b9472ee
park kang lim <gr1993@naver.com>
2023-11-21 16:16:37 +0900

    OEOS-249 [feat] 포인트 소멸예정 안내 이메일 발송 구현

--- src/main/java/icube/schedule/DscntSchedule.java
+++ src/main/java/icube/schedule/DscntSchedule.java
@@ -1,20 +1,27 @@
 package icube.schedule;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.egovframe.rte.fdl.string.EgovStringUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import icube.common.framework.abst.CommonAbstractController;
+import icube.common.mail.MailService;
+import icube.common.util.FileUtil;
 import icube.manage.mbr.mbr.biz.MbrService;
 import icube.manage.mbr.mbr.biz.MbrVO;
 import icube.manage.promotion.mlg.biz.MbrMlgService;
 import icube.manage.promotion.point.biz.MbrPointService;
+import icube.manage.promotion.point.biz.MbrPointVO;
 
 
 /**
@@ -34,6 +41,64 @@ public class DscntSchedule extends CommonAbstractController {
 	@Resource(name = "mbrMlgService")
 	private MbrMlgService mbrMlgService;
 
+	@Resource(name="mailService")
+	private MailService mailService;
+
+	@Value("#{props['Mail.Form.FilePath']}")
+	private String mailFormFilePath;
+
+	@Value("#{props['Mail.Username']}")
+	private String sendMail;
+
+	@Value("#{props['Mail.Testuser']}")
+	private String mailTestuser;
+
+	@Value("#{props['Profiles.Active']}")
+	private String activeMode;
+	
+	SimpleDateFormat format = new SimpleDateFormat("yyyy년 MM월 dd일");
+	
+	SimpleDateFormat format2 = new SimpleDateFormat("yyyy");
+	
+	/**
+	 * 소멸예정 포인트 안내
+	 */
+	@Scheduled(cron="0 0 9 30 11 *")
+	public void guideExtinctPoint() throws Exception {
+		log.debug("   ###   guideExtinctPoint START  ####");
+
+		List<MbrPointVO> mbrPointList = mbrPointService.selectExtinctPoinThisYear();
+		Date now = new Date();
+		String today = format.format(now);
+		String thisYear = format2.format(now);
+		
+		for(MbrPointVO mbrPointVO : mbrPointList) {
+			try {
+				String MAIL_FORM_PATH = mailFormFilePath;
+				String mailForm = FileUtil.readFile(MAIL_FORM_PATH+"mail_guide_extinct_point.html");
+				MbrVO mbrVO = mbrPointVO.getMbrList().get(0);
+				mailForm = mailForm.replace("((mbrNm))", mbrVO.getMbrNm());
+				mailForm = mailForm.replace("((today))", today);
+				mailForm = mailForm.replace("((point))", String.valueOf(mbrPointVO.getPointAcmtl()));
+				mailForm = mailForm.replace("((thisYear))", thisYear);
+				
+				// 메일 발송
+				String mailSj = "[이로움ON] 소멸예정 포인트 안내";
+				if(!EgovStringUtil.equals("local", activeMode)) {
+					mailService.sendMail(sendMail, mbrVO.getEml(), mailSj, mailForm);
+				} else {
+					mailService.sendMail(sendMail, this.mailTestuser, mailSj, mailForm); //테스트
+				}
+			}catch(Exception e) {
+				log.debug("   ###   guideExtinctPoint Error   ### : " + e.toString());
+				e.printStackTrace();
+			}
+		}
+
+		log.debug("   ###   guideExtinctPoint END  ####");
+	}
+	
+	
 	/**
 	 * 회원 포인트 자동 소멸
 	 */
