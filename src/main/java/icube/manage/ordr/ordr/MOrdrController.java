f7c0e52f3b257e28bbb477a6323c3dd2c1e0a7ad
liveonDylee <101084764+liveonDylee@users.noreply.github.com>
2023-12-07 11:21:41 +0900

    OEOS-215 > OEOS-323 > [마켓]주문목록 엑셀 다운로드시 양식 개선 요청

--- src/main/java/icube/manage/ordr/ordr/MOrdrController.java
+++ src/main/java/icube/manage/ordr/ordr/MOrdrController.java
@@ -1560,8 +1560,8 @@ public class MOrdrController extends CommonAbstractController {
         row1.createCell(cellPos++).setCellValue("수령인");
 		if (cellAdd231206 ) {
 			row1.createCell(cellPos++).setCellValue("수령인 휴대전화");
-			row1.createCell(cellPos++).setCellValue("배송주소");
 			row1.createCell(cellPos++).setCellValue("우편번호");
+			row1.createCell(cellPos++).setCellValue("배송주소");
 		}
 
         row1.createCell(cellPos++).setCellValue("상품구분");
@@ -1607,14 +1607,14 @@ public class MOrdrController extends CommonAbstractController {
 					dataRow.createCell(cellPos++).setCellValue("");//수령인 휴대폰번호
 				}
 				if (dataAdd231206Map.containsKey(ordrDtlVO.getSttsTy()) && dataAdd231206Map.get(ordrDtlVO.getSttsTy())){
-					dataRow.createCell(cellPos++).setCellValue(ordrDtlVO.getRecptrAddr() + " " + ordrDtlVO.getRecptrDaddr());//배송지
-				}else{
-					dataRow.createCell(cellPos++).setCellValue("");//배송지
-				}
-				if (dataAdd231206Map.containsKey(ordrDtlVO.getSttsTy()) && dataAdd231206Map.get(ordrDtlVO.getSttsTy())){
 					dataRow.createCell(cellPos++).setCellValue(ordrDtlVO.getRecptrZip());//우편번호
 				}else{
 					dataRow.createCell(cellPos++).setCellValue("");//우편번호
+				}
+				if (dataAdd231206Map.containsKey(ordrDtlVO.getSttsTy()) && dataAdd231206Map.get(ordrDtlVO.getSttsTy())){
+					dataRow.createCell(cellPos++).setCellValue(ordrDtlVO.getRecptrAddr() + " " + ordrDtlVO.getRecptrDaddr());//배송지
+				}else{
+					dataRow.createCell(cellPos++).setCellValue("");//배송지
 				}
 			}
 
