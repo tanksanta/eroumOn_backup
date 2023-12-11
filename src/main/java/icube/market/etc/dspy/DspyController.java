fcf57183e107c2145e2acb51b0dcfb08c8c4f67e
park kang lim <gr1993@naver.com>
2023-12-11 12:05:25 +0900

    OEOS-340 [feat] 마켓 메인 > 기획전 > 기획전 상세에 상품 가격, 찜하기, 장바구니 담기 구현
    (cherry picked from commit cac1af3878fa8d9a75bb6959a4e7dca269b001df)

--- src/main/java/icube/market/etc/dspy/DspyController.java
+++ src/main/java/icube/market/etc/dspy/DspyController.java
@@ -1,5 +1,6 @@
 package icube.market.etc.dspy;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +8,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.egovframe.rte.fdl.string.EgovStringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +16,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import icube.common.framework.abst.CommonAbstractController;
 import icube.common.vo.CommonListVO;
+import icube.manage.mbr.itrst.biz.WishService;
+import icube.manage.mbr.itrst.biz.WishVO;
 import icube.manage.promotion.dspy.biz.PlanngDspyGrpGdsService;
+import icube.manage.promotion.dspy.biz.PlanngDspyGrpGdsVO;
 import icube.manage.promotion.dspy.biz.PlanngDspyGrpService;
 import icube.manage.promotion.dspy.biz.PlanngDspyGrpVO;
 import icube.manage.promotion.dspy.biz.PlanningDspyService;
 import icube.manage.promotion.dspy.biz.PlanningDspyVO;
+import icube.market.mbr.biz.MbrSession;
 
 /**
  * 이로움 기획전
@@ -37,6 +43,13 @@ public class DspyController extends CommonAbstractController{
 
 	@Resource(name = "planngDspyGrpGdsService")
 	private PlanngDspyGrpGdsService pdgsGdsService;
+	
+	@Resource(name = "wishService")
+	private WishService wishService;
+	
+	@Autowired
+	private MbrSession mbrSession;
+	
 
 	/**
 	 * 기획전 > 리스트
@@ -82,7 +95,24 @@ public class DspyController extends CommonAbstractController{
 
 		//연관상품 그룹
 		List <PlanngDspyGrpVO> dspyGrpList =  pdgsService.selectGrpList(planngDspyNo);
-
+		
+		//회원의 위시정보 추가
+		if (mbrSession.isLoginCheck()) {
+			Map<String, Object> paramMap = new HashMap<String, Object>();
+			paramMap.put("srchUniqueId", mbrSession.getUniqueId());
+			List<WishVO> wishList = wishService.selectWishListAll(paramMap);
+			
+			if (wishList.size() > 0) {
+				for (PlanngDspyGrpVO dspyGrp : dspyGrpList) {
+					for (PlanngDspyGrpGdsVO dspyGrpGds : dspyGrp.getGrpGdsList()) {
+						if (wishList.stream().anyMatch(f -> f.getGdsNo() == dspyGrpGds.getGdsNo())) {
+							dspyGrpGds.setWishYn(1);
+						}
+					}
+				}
+			}
+		}
+		
 		model.addAttribute("dspyVO", PlanningDspyVO);
 		model.addAttribute("dspyGrpList", dspyGrpList);
 
