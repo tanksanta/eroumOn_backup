d2a546b88e41837e108c08908785e186e6d2c44a
liveonDylee <101084764+liveonDylee@users.noreply.github.com>
2024-01-09 10:32:38 +0900

    상품 > 묶음배송 그룹관리 > 리스트 버그 수정

--- src/main/java/icube/manage/sysmng/entrps/MEntrpsDlvyGrpController.java
+++ src/main/java/icube/manage/sysmng/entrps/MEntrpsDlvyGrpController.java
@@ -64,9 +64,9 @@ public class MEntrpsDlvyGrpController extends CommonAbstractController {
 		if (reqMap.get("srchTarget") != null && !EgovStringUtil.equals(reqMap.get("srchTarget").toString(), "")){
 			entrpsNo = Integer.parseInt(reqMap.get("srchTarget").toString());
 			listVO.setParam("entrpsNo", entrpsNo);
-		}else  if ( entrpsListVO.getListObject().size() > 0){
-			entrpsNo = ((EntrpsVO) (entrpsListVO.getListObject().get(0))).getEntrpsNo();
-			listVO.setParam("entrpsNo", entrpsNo);
+//		}else  if ( entrpsListVO.getListObject().size() > 0){
+//			entrpsNo = ((EntrpsVO) (entrpsListVO.getListObject().get(0))).getEntrpsNo();
+//			listVO.setParam("entrpsNo", entrpsNo);
 		}
 
 		listVO = entrpsDlvyGrpService.entrpsDlvyGrpListVO(listVO);
