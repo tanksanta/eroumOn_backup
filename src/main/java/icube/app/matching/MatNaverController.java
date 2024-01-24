0e48c14bb18d1b26aab86ff226f42f1ba4e18573
park kang lim <gr1993@naver.com>
2024-01-24 13:59:03 +0900

    [feat] 매칭앱 카카오, 네이버 로그인 적용

--- src/main/java/icube/app/matching/MatNaverController.java
+++ src/main/java/icube/app/matching/MatNaverController.java
@@ -8,36 +8,36 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.View;
 
-import icube.common.api.biz.KakaoApiService;
+import icube.common.api.biz.NaverApiService;
 import icube.common.framework.abst.CommonAbstractController;
 import icube.common.framework.view.JavaScript;
 import icube.common.framework.view.JavaScriptView;
 
 /**
- * 매칭앱 - 카카오 로그인
+ * 매칭앱 - 네이버 로그인
  */
 @Controller
-@RequestMapping(value="#{props['Globals.Matching.path']}/kakao")
-public class MatKakaoController extends CommonAbstractController {
+@RequestMapping(value="#{props['Globals.Matching.path']}/naver")
+public class MatNaverController extends CommonAbstractController {
 	
-	@Resource(name = "kakaoApiService")
-	private KakaoApiService kakaoApiService;
+	@Resource(name = "naverApiService")
+	private NaverApiService naverApiService;
 	
 	@Value("#{props['Globals.Matching.path']}")
 	private String matchingPath;
 	
 	
-	@RequestMapping(value = "/auth")
+	@RequestMapping(value = "/get")
 	public View auth(
 			HttpSession session
 			)throws Exception {
 
 		JavaScript javaScript = new JavaScript();
-		String kakaoUrl = kakaoApiService.getKakaoUrl();
+		String getUrl = naverApiService.getUrl();
 		
 		session.setAttribute("prevSnsPath", "matching");
 
-		javaScript.setLocation(kakaoUrl);
+		javaScript.setLocation(getUrl);
 		return new JavaScriptView(javaScript);
 	}
 }
