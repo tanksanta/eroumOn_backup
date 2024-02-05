2f1d6ea58fbfadbbd73855821b85552de07d4ae2
park kang lim <gr1993@naver.com>
2024-02-05 11:59:22 +0900

    [feat] 앱설정 테이블 스키마 및 DAO 구현

---
+++ src/main/java/icube/manage/mbr/mbr/biz/MbrAppSettingDAO.java
@@ -0,0 +1,31 @@
+package icube.manage.mbr.mbr.biz;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import icube.common.framework.abst.CommonAbstractMapper;
+
+@Repository("mbrAppSettingDAO")
+public class MbrAppSettingDAO extends CommonAbstractMapper {
+	
+	public List<MbrAppSettingVO> selectMbrAppSettingListAll(Map<String, Object> paramMap) throws Exception {
+		return selectList("mbr.app.setting.selectMbrAppSetting", paramMap);
+	}
+	
+	public MbrAppSettingVO selectMbrAppSettingByMbrUniqueId(String uniqueId) throws Exception {
+		Map<String, Object> paramMap = new HashMap<>();
+		paramMap.put("srchMbrUniqueId", uniqueId);
+		return selectOne("mbr.app.setting.selectMbrAppSetting", paramMap);
+	}
+	
+	public void insertMbrAppSetting(MbrAppSettingVO mbrAppSettingVO) throws Exception {
+		insert("mbr.app.setting.insertMbrAppSetting", mbrAppSettingVO);
+	}
+	
+	public void updateMbrAppSetting(MbrAppSettingVO mbrAppSettingVO) throws Exception {
+		update("mbr.app.setting.updateMbrAppSetting", mbrAppSettingVO);
+	}
+}
