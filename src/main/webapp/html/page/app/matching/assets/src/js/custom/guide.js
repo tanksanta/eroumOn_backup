f96448c6087baa265ccd137338cdb107a560642d
gogosing1234-admin <gogosing1234@thkc.co.kr>
2024-02-16 14:24:35 +0900

    [퍼블] 매칭앱 추가요청사항 외
    - 유효성검사(F_SGN_002_join0107.html)
    - 중간작업
    - 컴포넌트 작업

--- src/main/webapp/html/page/app/matching/assets/src/js/custom/guide.js
+++ src/main/webapp/html/page/app/matching/assets/src/js/custom/guide.js
@@ -10,14 +10,35 @@ $(function(){
     //
     $('.broad_area li').click(function(){
 
+        var thisTxt = $(this).text();
+
+        $('.broad_evt').text(thisTxt);
+
         $('.broad_area li').removeClass('active');
         $(this).addClass('active');
     });
 
 
 
+    //회원가입 전체동의 체크박스
+    $('.total_evt').on('click', function() {
+        
+        if($(this).is(':checked')==true){
+            $('.group_chk_area :checkbox').prop('checked', true);				
+        }
 
+        else{
+            $('.group_chk_area :checkbox').prop('checked', false);	
+        }
 
+    });
+    $(".group_chk_area :checkbox").click(function() {
+        var total = $(".group_chk_area :checkbox").length;
+        var checked = $(".group_chk_area :checkbox:checked").length;
+        
+        if(total != checked) $(".total_evt").prop("checked", false);
+        else $(".total_evt").prop("checked", true); 
+    });
 
 
 
