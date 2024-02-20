5a1d1eadfe4287c775d14e7f79f72d1aae678e67
gogosing1234-admin <gogosing1234@thkc.co.kr>
2024-02-20 15:08:33 +0900

    [퍼블] 중간작업 - 어르신등록 - css 수정 - 상담신청(미완성)

--- src/main/webapp/html/page/app/matching/assets/src/js/custom/guide.js
+++ src/main/webapp/html/page/app/matching/assets/src/js/custom/guide.js
@@ -40,8 +40,42 @@ $(function(){
     });
 
 
+    //어르신등록 - 가족관계
+    $('.family_tree_area .item').click(function(){
+
+        $('.family_tree_area .item').removeClass('active');
+        $(this).addClass('active');
+
+        //본인 선택시 버튼 '등록하기'로 변경
+        if($(this).hasClass('me')===false){
+
+            $('.btnEvt_me').text('다음')
+        }
+        else{
+            $('.btnEvt_me').text('등록하기');
+        }
+    
+    });
+    
+
+    //시도 지역선택 chip
+    $('.chip_area li').click(function(){
+
+        $('.chip_area li').removeClass('active');
+        $(this).addClass('active');
+
+    });
+
+
+
+
 
 });
+
+
+
+
+
 
 //maxlength 넘버제한 처리
 function maxLengthCheck(object){
@@ -49,3 +83,6 @@ function maxLengthCheck(object){
       object.value = object.value.slice(0, object.maxLength);
     }    
   }
+
+
+  
